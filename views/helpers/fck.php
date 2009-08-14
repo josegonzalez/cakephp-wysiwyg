c7ad85365ed513f58760a7be44762f0d8e75c0a9
Jose Gonzalez <myaccounts@savant.be>
2009-08-14 07:41:19 -0400

    refactoring helpers

--- views/helpers/fck.php
+++ views/helpers/fck.php
@@ -1,18 +1,42 @@
 <?php
 /**
  * FCKHelper is a helper for FCKEditor.
- * This helper REQUIRES the FCKEditor.
+ * This helper REQUIRES the FCKEditor installation files.
  *
- * @author: Jose Diaz-Gonzalez
- * @version: 1.0
- * @email: support@savant.be
- * @site: http://josediazgonzalez.com
+ * @package       cake
+ * @subpackage    cake.app.plugins.wysiwyg.views.helpers
+ * @author:       Jose Diaz-Gonzalez
+ * @version:      1.1
+ * @email:        support@savant.be
+ * @site:         http://josediazgonzalez.com
  */
 class FckHelper extends AppHelper {
-// Take advantage of other helpers
-	var $helpers = array('Html', 'Form', 'Javascript');
-// Check if the tiny_mce.js file has been added or not
-	var $_script = false;
+/**
+ * Helper dependencies
+ *
+ * @var array
+ */
+	var $helpers = array('Form', 'Html', 'Javascript');
+/**
+ * Whether helper has been initialized once or not
+ *
+ * @var boolean
+ */
+	var $_initialized = false;
+
+/**
+ * Holds the default model for the helper
+ *
+ * @var string
+ */
+	var $__defaultModel = null;
+
+/**
+ * Holds the model/field pair for the current element
+ *
+ * @var array
+ */
+	var $__modelFieldPair = null;
 
 /**
  * Adds the fckeditor.js file and constructs the options
@@ -21,8 +45,9 @@ class FckHelper extends AppHelper {
  * @param array $fckoptions Array of FckEditor attributes for this textarea
  * @return string JavaScript code to initialise the FckEditor area
  */
-	function _build($field, $fckOptions = array()) {
-		$fckOptions = array_merge(
+	function _build($field, $options = array()) {
+		$modelFieldPair = $this->__field($field);
+		$options = array_merge(
 			array(
 				'basepath' => $this->Html->base,
 				'fckPath' => '/js/fckeditor/',
@@ -30,21 +55,21 @@ class FckHelper extends AppHelper {
 				'toolbarSet' => 'Default',
 				'skinPath' => 'editor/skins/',
 				'skin' => 'default'),
-			$fckOptions);
+			$options);
 
-		if (!$this->_script) {
+		if (!$this->_initialized) {
 			// We don't want to add this every time, it's only needed once
-			$this->_script = true;
+			$this->_initialized = true;
 			$this->Javascript->link('fckeditor/fckeditor.js', false);
 		}
 
 		return $this->Javascript->codeBlock(
 			"window.onload = function() {
-				var oFCKeditor = new FCKeditor('data[" . strtok($field, '.') . "][" . strtok('.') . "]');
-				oFCKeditor.BasePath = \"" . $fckOptions['basepath'] . $fckOptions['fckPath'] . "\";
-				oFCKeditor.Config['SkinPath'] = \"" . $fckOptions['fckPath'] . $fckOptions['skinPath'] . $fckOptions['skin'] . '/' . "\";
-				oFCKeditor.Value = \"" . $fckOptions['content'] . "\";
-				oFCKeditor.ToolbarSet = \"" . $fckOptions['toolbarSet'] . "\";
+				var oFCKeditor = new FCKeditor('data[" . $modelFieldPair['model'] . "][" . $modelFieldPair['field'] . "]');
+				oFCKeditor.BasePath = \"" . $options['basepath'] . $options['fckPath'] . "\";
+				oFCKeditor.Config['SkinPath'] = \"" . $options['fckPath'] . $options['skinPath'] . $options['skin'] . '/' . "\";
+				oFCKeditor.Value = \"" . $options['content'] . "\";
+				oFCKeditor.ToolbarSet = \"" . $options['toolbarSet'] . "\";
 				oFCKeditor.ReplaceTextarea();
 			}
 			", array('safe' => false, 'inline' => false)
@@ -56,7 +81,7 @@ class FckHelper extends AppHelper {
 *
 * @param string $field - used to build input name for views, 
 * @param array $options Array of HTML attributes.
-* @param array $fckOptions Array of FckEditor attributes for this textarea
+* @param array $fckOptions Array of FckEditor attributes for this input field
 * @return string An HTML input element with FckEditor
 */
 	function input($field = null, $options = array(), $fckOptions = array()){
@@ -73,6 +98,46 @@ class FckHelper extends AppHelper {
 */
 	function textarea($field = null, $options = array(), $fckOptions = array()){
 		return $this->Form->textarea($field, $options) . $this->_build($field, $fckOptions);
+	}
+
+/**
+ * Returns the modelname and fieldname for the current string
+ *
+ * @return mixed Array containing modelName and/or fieldName, null in all other cases
+ * @author Jose Diaz-Gonzalez
+ **/
+	function __field($field = null) {
+		if (empty($field)) {
+			return null;
+		}
+
+		$firstToken = strtok($field, '.');
+		$secondToken = strtok('.');
+		if($secondToken === false) {
+			$modelName = $this->__defaultModel();
+			$fieldName = $firstToken;
+		} else {
+			$modelName = $firstToken;
+			$fieldName = $secondToken;
+		}
+		$this->__modelFieldPair = array('model' => $modelName, 'field' => $fieldName);
+		return $this->__modelFieldPair;
+	}
+
+/**
+ * Gets the default model of the paged sets
+ *
+ * @return string Model name or the inflected controller name if the model isn't initialized. Null in all other cases
+ */
+	function __defaultModel() {
+		if ($this->__defaultModel != null) {
+			return $this->__defaultModel;
+		} else {
+			if (!empty($this->params['controller'])) {
+				$this->__defaultModel = Inflector::classify($this->params['controller']);
+			}
+		}
+		return $this->__defaultModel;
 	}
 }
 ?>
--- views/helpers/tinymce.php
+++ views/helpers/tinymce.php
@@ -1,44 +1,72 @@
 <?php
 /**
  * TinyMCEHelper is a helper for TinyMCE
- * This helper REQUIRES the TinyMCE installation.
+ * This helper REQUIRES the TinyMCE installation files.
  * Based on David Boyer's helper at http://bakery.cakephp.org/articles/view/tinymce-helper-1
  * 
- * @author: Jose Diaz-Gonzalez
- * @version: 1.1
- * @email: support@savant.be
- * @site: http://josediazgonzalez.com
+ * @package       cake
+ * @subpackage    cake.app.plugins.wysiwyg.views.helpers
+ * @author:       Jose Diaz-Gonzalez
+ * @version:      1.2
+ * @email:        support@savant.be
+ * @site:         http://josediazgonzalez.com
  */
-class TinyMceHelper extends AppHelper {
-// Take advantage of other helpers
-	var $helpers = array('Javascript', 'Form');
-// Check if the tiny_mce.js file has been added or not
-	var $_script = false;
-// This keeps track of the times the helper was used
-	var $_used = 0;
+class TinymceHelper extends AppHelper {
+/**
+ * Helper dependencies
+ *
+ * @var array
+ */
+	var $helpers = array('Form', 'Javascript');
+
+/**
+ * Whether helper has been initialized once or not
+ *
+ * @var boolean
+ */
+	var $_initialized = false;
+
+/**
+ * Holds the default model for the helper
+ *
+ * @var string
+ */
+	var $__defaultModel = null;
+
+/**
+ * Holds the model/field pair for the current element
+ *
+ * @var array
+ */
+	var $__modelFieldPair = null;
 
 /**
  * Adds the tiny_mce.js file and constructs the options
  *
  * @param string $fieldName Name of a field, like this "Modelname.fieldname", "Modelname/fieldname" is deprecated
+ * @param array $options Array of HTML attributes.
  * @param array $tinyoptions Array of TinyMCE attributes for this textarea
  * @return string JavaScript code to initialise the TinyMCE area
  */
-	function _build($fieldName, $tinyoptions = array()) {
-		if (!$this->_script) {
+	function _build($field, $options = array()) {
+		$selector = "data[{$this->__modelFieldPair['model']}][{$this->__modelFieldPair['field']}]";
+
+		if (!$this->_initialized) {
 			// We don't want to add this every time, it's only needed once
-			$this->_script = true;
+			$this->_initialized = true;
 			$this->Javascript->link('tiny_mce/tiny_mce_src.js', false);
 		}
+
 		// Ties the options to the field
-		$tinyoptions['mode'] = 'textareas';
-		$tinyoptions['elements'] = $this->__name($fieldName);
-		$tinyoptions['skin'] = 'o2k7';
-		$tinyoptions['theme'] = 'advanced';
-		$tinyoptions['file_browser_callback'] = 'fileBrowserCallBack';
-		$tinyoptions['plugins'] = 'advhr,preview';
-		$tinyoptions['theme_advanced_buttons3_add'] = 'advhr,preview';
-		$tinyoptions['extended_valid_elements'] = 'hr[class|width|size|noshade]';
+		$options['mode'] = 'textareas';
+		$options['editor_selector'] = $selector;
+		$options['elements'] = $this->__name($field);
+		$options['skin'] = 'default';
+		$options['theme'] = 'advanced';
+		$options['file_browser_callback'] = 'fileBrowserCallBack';
+		$options['plugins'] = 'advhr,preview';
+		$options['theme_advanced_buttons3_add'] = 'advhr,preview';
+		$options['extended_valid_elements'] = 'hr[class|width|size|noshade]';
 		
 		echo $this->Javascript->codeBlock(
 			"function fileBrowserCallBack(field_name, url, type, win) {
@@ -47,7 +75,7 @@ class TinyMceHelper extends AppHelper {
 				window.open('".Helper::url(array('controller' => 'images', 'action' => 'tinymce'))."', 'browserWindow', 'modal,width=600,height=400,scrollbars=yes');
 			}"
 		);
-		return $this->Javascript->codeBlock('tinyMCE.init(' . $this->Javascript->object($tinyoptions) . ');');
+		return $this->Javascript->codeBlock('tinyMCE.init(' . $this->Javascript->object($options) . ');');
 	}
 
 /**
@@ -58,34 +86,80 @@ class TinyMceHelper extends AppHelper {
  * @param array $tinyoptions Array of TinyMCE attributes for this textarea
  * @return string An HTML textarea element with TinyMCE
  */
-	function textarea($fieldName, $options = array(), $tinyoptions = array()) {
-		$options['type'] = 'textareas';
-		$selector = "mceTextArea{$this->_used}";
-		$tinyoptions = array(
-			'editor_selector' => $selector,
-			'mode' => "specific_textareas"
-		);
-		$options['class'] = (isset($options['class'])) ? $options['class'].$selector : $options['class'] = $selector;
-		return $this->Form->textarea($fieldName, $options) . $this->_build($fieldName, $tinyoptions);
-	}
-
-/**
- * Creates a TinyMCE textarea.
- *
- * @param string $fieldName Name of a field, like this "Modelname.fieldname", "Modelname/fieldname" is deprecated
- * @param array $options Array of HTML attributes.
- * @param array $tinyoptions Array of TinyMCE attributes for this textarea
- * @return string An HTML textarea element with TinyMCE
- */
-	function input($fieldName, $options = array(), $tinyoptions = array()) {
+	function input($field, $options = array(), $tinyoptions = array()) {
 		$options['type'] = 'textarea';
-		$selector = "mceInputArea{$this->_used}";
-		$tinyoptions = array(
-			'editor_selector' => $selector,
-			'mode' => "specific_textareas"
-		);
-		$options['class'] = (isset($options['class'])) ? $options['class'].$selector : $options['class'] = $selector;
-		return $this->Form->input($fieldName, $options) . $this->_build($fieldName, $tinyoptions);
+		$this->__field($field);
+		$selector = "data[{$this->__modelFieldPair['model']}][{$this->__modelFieldPair['field']}]";
+
+		if (isset($options['class'])) {
+			$options['class'] .= $selector;
+		} else {
+			$options['class'] = $selector;
+		}
+
+		return $this->Form->input($field, $options) . $this->_build($field, $tinyoptions);
+	}
+
+/**
+ * Creates a TinyMCE textarea.
+ *
+ * @param string $fieldName Name of a field, like this "Modelname.fieldname", "Modelname/fieldname" is deprecated
+ * @param array $options Array of HTML attributes.
+ * @param array $tinyoptions Array of TinyMCE attributes for this textarea
+ * @return string An HTML textarea element with TinyMCE
+ */
+	function textarea($field, $options = array(), $tinyoptions = array()) {
+		$options['type'] = 'textareas';
+		$this->__field($field);
+		$selector = "data[{$this->__modelFieldPair['model']}][{$this->__modelFieldPair['field']}]";
+
+		if (isset($options['class'])) {
+			$options['class'] .= $selector;
+		} else {
+			$options['class'] = $selector;
+		}
+
+		return $this->Form->textarea($field, $options) . $this->_build($field, $tinyoptions);
+	}
+
+/**
+ * Returns the modelname and fieldname for the current string
+ *
+ * @return mixed Array containing modelName and/or fieldName, null in all other cases
+ * @author Jose Diaz-Gonzalez
+ **/
+	function __field($field = null) {
+		if (empty($field)) {
+			return null;
+		}
+
+		$firstToken = strtok($field, '.');
+		$secondToken = strtok('.');
+		if($secondToken === false) {
+			$modelName = $this->__defaultModel();
+			$fieldName = $firstToken;
+		} else {
+			$modelName = $firstToken;
+			$fieldName = $secondToken;
+		}
+		$this->__modelFieldPair = array('model' => $modelName, 'field' => $fieldName);
+		return $this->__modelFieldPair;
+	}
+
+/**
+ * Gets the default model of the paged sets
+ *
+ * @return string Model name or the inflected controller name if the model isn't initialized. Null in all other cases
+ */
+	function __defaultModel() {
+		if ($this->__defaultModel != null) {
+			return $this->__defaultModel;
+		} else {
+			if (!empty($this->params['controller'])) {
+				$this->__defaultModel = Inflector::classify($this->params['controller']);
+			}
+		}
+		return $this->__defaultModel;
 	}
 }
 ?>
