07b7822172e5f06508de5c450a3b55591e81a3c7
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-07-02 01:52:40 -0400

    Fixed TinyMCEHelper and simplified code for TinyMCE4. Closes #7

--- View/Helper/TinymceHelper.php
+++ View/Helper/TinymceHelper.php
@@ -15,6 +15,24 @@ App::uses('WysiwygAppHelper', 'Wysiwyg.View/Helper');
 
 class TinymceHelper extends WysiwygAppHelper {
 
+
+	protected function _initialize($options) {
+		if ($this->_initialized) {
+			return;
+		}
+
+		$options = array_merge(array(
+			'scriptPath' => 'tinymce/tinymce.min.js',
+		), $options);
+
+		if (!$options['scriptPath']) {
+			return;
+		}
+
+		$this->_initialized = true;
+		$this->Html->script($options['scriptPath'], false);
+	}
+
 /**
  * Adds the tiny_mce.js file and constructs the options
  *
@@ -24,75 +42,28 @@ class TinymceHelper extends WysiwygAppHelper {
  * @return string JavaScript code to initialise the TinyMCE area
  */
 	protected function _build($field, $options = array()) {
-		$field = $this->_field($field);
-
-		if (!$this->_initialized) {
-			$this->_initialized = true;
-			$this->Html->link('tiny_mce/tiny_mce_src.js', false);
-		}
-
-		// Ties the options to the field
-		$options['editor_selector'] = "data[{$field['modelName']}][{$field['fieldName']}]";
-		$options['elements'] = $this->_name($field);
-
 		$options = array_merge(array(
-			'mode' => 'textareas',
-			'theme' => 'advanced',
-			'skin' => 'default',
-			'theme_advanced_buttons1' => 'bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,undo,redo,link,unlink,image',
-			'theme_advanced_buttons2' => '',
-			'theme_advanced_buttons3' => '',
-			'theme_advanced_toolbar_location' => 'top',
-			'theme_advanced_toolbar_align' => 'left',
-			'theme_advanced_statusbar_location' => 'bottom',
-			'plugins' => 'inlinepopups',
+			'bufferScript' => false,
+			'scriptPath' => 'tinymce/tinymce.min.js',
 		), $options);
 
-		return $this->Html->scriptBlock('tinyMCE.init(' . json_encode($options) . ');');
-	}
+		$this->_initialize($options);
 
-/**
- * Creates a TinyMCE textarea.
- *
- * @param string $fieldName Name of a field, like this "Modelname.fieldname", "Modelname/fieldname" is deprecated
- * @param array $options Array of HTML attributes.
- * @param array $tinyoptions Array of TinyMCE attributes for this textarea
- * @return string An HTML textarea element with TinyMCE
- */
-	public function input($field, $options = array(), $tinyoptions = array()) {
-		$options['type'] = 'textarea';
-		$this->_field($field);
-		$selector = "data[{$this->_modelFieldPair['model']}][{$this->_modelFieldPair['field']}]";
+		$domId = $this->domId($field);
+		$options['selector'] = "#{$domId}";
+		$initOptions = json_encode(array_diff_key($options, array(
+			'bufferScript' => true,
+			'scriptPath' => true,
+		)));
 
-		if (isset($options['class'])) {
-			$options['class'] .= ' ' . $selector;
-		} else {
-			$options['class'] = $selector;
+		$script = 'tinyMCE.init(' . $initOptions . ');';
+
+		if (!empty($options['bufferScript'])) {
+			$this->Js->buffer($script);
+			return '';
 		}
 
-		return $this->Form->input($field, $options) . $this->_build($field, $tinyoptions);
-	}
-
-/**
- * Creates a TinyMCE textarea.
- *
- * @param string $fieldName Name of a field, like this "Modelname.fieldname", "Modelname/fieldname" is deprecated
- * @param array $options Array of HTML attributes.
- * @param array $tinyoptions Array of TinyMCE attributes for this textarea
- * @return string An HTML textarea element with TinyMCE
- */
-	public function textarea($field, $options = array(), $tinyoptions = array()) {
-		$options['type'] = 'textarea';
-		$this->_field($field);
-		$selector = "data[{$this->_modelFieldPair['model']}][{$this->_modelFieldPair['field']}]";
-
-		if (isset($options['class'])) {
-			$options['class'] .= ' ' . $selector;
-		} else {
-			$options['class'] = $selector;
-		}
-
-		return $this->Form->textarea($field, $options) . $this->_build($field, $tinyoptions);
+		return $this->Html->scriptBlock($script, array('safe' => true));
 	}
 
 }
