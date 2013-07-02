1f76b96227c0d8b5dd04737027cd3fb84daab166
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-07-02 01:52:59 -0400

    remove dead code

--- View/Helper/WysiwygAppHelper.php
+++ View/Helper/WysiwygAppHelper.php
@@ -46,20 +46,6 @@ class WysiwygAppHelper extends AppHelper {
 	protected $_initialized = false;
 
 /**
- * Holds the default model for the helper
- *
- * @var string
- */
-	protected $_defaultModel = null;
-
-/**
- * Holds the model/field pair for the current element
- *
- * @var array
- */
-	protected $_modelFieldPair = array();
-
-/**
  * Creates an fckeditor input field
  *
  * @param string $field - used to build input name for views,
@@ -81,50 +67,6 @@ class WysiwygAppHelper extends AppHelper {
  */
 	public function textarea($fieldName = null, $options = array(), $helperOptions = array()) {
 		return $this->Form->textarea($fieldName, $options) . $this->_build($fieldName, $helperOptions);
-	}
-
-/**
- * Returns the modelname and fieldname for the current string
- *
- * @return mixed Array containing modelName and/or fieldName, null in all other cases
- * @author Jose Diaz-Gonzalez
- **/
-	protected function _field($field = null) {
-		if (isset($this->_modelFieldPair[$field])) {
-			return $this->_modelFieldPair[$field];
-		}
-
-		if (empty($field)) {
-			return null;
-		}
-
-		$firstToken = strtok($field, '.');
-		$secondToken = strtok('.');
-		if ($secondToken === false) {
-			$modelName = $this->_defaultModel();
-			$fieldName = $firstToken;
-		} else {
-			$modelName = $firstToken;
-			$fieldName = $secondToken;
-		}
-		return $this->_modelFieldPair[$field] = compact('modelName', 'fieldName');
-	}
-
-/**
- * Gets the default model of the paged sets
- *
- * @return string Model name or the inflected controller name if the model isn't initialized. Null in all other cases
- */
-	protected function _defaultModel() {
-		if ($this->_defaultModel != null) {
-			return $this->_defaultModel;
-		}
-
-		if (!empty($this->params['controller'])) {
-			$this->_defaultModel = Inflector::classify($this->params['controller']);
-		}
-
-		return $this->_defaultModel;
 	}
 
 }
